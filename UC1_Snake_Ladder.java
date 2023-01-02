b63f6ebdaa5b4f0908c8c62805793e77e093888b
vaibhavbhagadia@1999 <vaibhavbhagadia68@gmail.com>
2023-01-02 23:30:15 +0530

    Start Of The Game

---
+++ UC1_Snake_Ladder.java
@@ -0,0 +1,13 @@
+package Day4Assignment;
+
+public class UC1_Snake_Ladder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int position = 0;
+		System.out.println("welcom player 1");
+		System.out.println("you are at the position:" +position );
+	}
+
+}
