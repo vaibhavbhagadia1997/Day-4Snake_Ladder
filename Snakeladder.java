6bca3e8ed7cd646ef39f8eb64223224bad5e371b
vaibhavbhagadia@1999 <vaibhavbhagadia68@gmail.com>
2023-01-02 23:26:40 +0530

    Added:Greetings

---
+++ Snakeladder.java
@@ -0,0 +1,11 @@
+package Day4Assignment;
+
+public class Snakeladder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("SNAKE LADDER GAME");
+	}
+
+}
